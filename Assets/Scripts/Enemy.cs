7b133802b4cb3cc28487af6698efb7a856f2ef88
Poornajith <chiranthapoornajith@gmail.com>
2025-03-27 11:03:04 +0530

    Enemy collide main ship effects

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,18 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private float deactivateX = -10f;
 
+    private ParticlePool particlePool;
+
+    private void Start()
+    {
+        particlePool = GameObject.Find("EnemyExplosions").GetComponent<ParticlePool>();
+    }
     private void Update()
     {
         if (transform.position.x < deactivateX)
         {
+            particlePool.PlayParticle(transform.position);
+            AudioManager.instance.PlayExplosionSound();
             gameObject.SetActive(false);
             PlayerHealth.instance.TakeDamage(10);
         }
